8d0b76aff4dff5ef618b5c12a7198088016b728f
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-03-05 14:57:38 -0500

    first

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <> 
+    <Navbar />
+    <Jumbotron />
+    </>,
+     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
