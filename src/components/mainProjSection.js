d49d5b7132835110d2dcaddfd4584a4e2f7d6ed0
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-03-06 10:28:35 -0500

    lala

--- src/components/mainProjSection.js
+++ src/components/mainProjSection.js
@@ -14,7 +14,7 @@ function MainProjSection() {
           </div>
         </div>
         <div className="col-md-4">
-          <img src="https://scontent-mia3-2.xx.fbcdn.net/v/t31.0-8/p960x960/17158945_606471952877226_3691054899361696034_o.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=QyuOng2lSg4AX-EEsOo&_nc_ht=scontent-mia3-2.xx&_nc_tp=6&oh=b7004c88280fbb3b805faa534acd513a&oe=5E917869" className="card-img" alt="..." />
+          <img src="https://scontent-mia3-2.xx.fbcdn.net/v/t31.0-8/p960x960/17158945_606471952877226_3691054899361696034_o.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=QyuOng2lSg4AX-EEsOo&_nc_ht=scontent-mia3-2.xx&_nc_tp=6&oh=b7004c88280fbb3b805faa534acd513a&oe=5E917869" className="card-img" alt="/" />
         </div>
       </div>
     </div>
