826da5fb580cb764f11e338d3fbc549e708ef008
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-03-06 09:19:34 -0500

    className

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,10 +3,10 @@ import '../styles/Jumbotron.css';
 
 function Jumbotron() {
   return (
-    <div class="jumbotron jumbotron-fluid text-center mainJumbo">
-      <div class="container">
-        <h1 class="display-4 jumbomain">HELLO!</h1>
-        <p class="lead jumbosup">My name is Jason Martinez<br/> I am a full-stack web developer with experience in HTML, CSS, JS, React.Js, Sass, Flux, Python, Flask, Node.Js, Bootstrap, UX/UI, MaterializeCSS, SQL, and MySQL.</p>
+    <div className="jumbotron jumbotron-fluid text-center mainJumbo">
+      <div className="container">
+        <h1 className="display-4 jumbomain">HELLO!</h1>
+        <p className="lead jumbosup">My name is Jason Martinez<br/> I am a full-stack web developer with experience in HTML, CSS, JS, React.Js, Sass, Flux, Python, Flask, Node.Js, Bootstrap, UX/UI, MaterializeCSS, SQL, and MySQL.</p>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,22 @@ import '../styles/Navbar.css';
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg">
-      <a class="navbar-brand name" href="#">Jason F. Martinez</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbartoggle"><i class="fas fa-bars"></i></span>
+    <nav className="navbar navbar-expand-lg">
+      <a className="navbar-brand name" href="#">Jason F. Martinez</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbartoggle"><i className="fas fa-bars"></i></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav ml-auto mr-2 mt-2 mt-lg-0 links">
-          <li class="nav-item active">
-            <a class="nav-link links" href="#">Home <span class="sr-only">(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav ml-auto mr-2 mt-2 mt-lg-0 links">
+          <li className="nav-item active">
+            <a className="nav-link links" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link links" href="#">Link</a>
+          <li className="nav-item">
+            <a className="nav-link links" href="#">Link</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link links" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <li className="nav-item">
+            <a className="nav-link links" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
           </li>
         </ul>
       </div>
