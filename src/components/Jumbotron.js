dc2b9c8c852abf53dd39112c3ddc8169020cdb7e
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-03-18 19:12:31 +0000

    fix

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,7 +6,8 @@ function Jumbotron() {
     <div className="jumbotron jumbotron-fluid text-center mainJumbo">
       <div className="container">
         <h1 className="display-4 jumbomain">HELLO!</h1>
-        <p className="lead jumbosup">My name is Jason Martinez<br/> I am a full-stack web developer with experience in HTML, CSS, JS, React.Js, Sass, Flux, Python, Flask, Node.Js, Bootstrap, UX/UI, MaterializeCSS, SQL, and MySQL.</p>
+        <p className="lead jumbosup d-none d-md-block d-lg-block">My name is Jason Martinez<br/> I am a full-stack web developer with experience in HTML, CSS, JS, React.Js, Sass, Flux, Python, Flask, Node.Js, Bootstrap, UX/UI, MaterializeCSS, SQL, and MySQL.</p>
+        <p className="lead jumbosup d-md-none d-lg-none">My name is Jason Martinez<br/> I am a full-stack web developer</p>
       </div>
     </div>
   );
